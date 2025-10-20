f68bb1779d93c042023211c9cb657382efd5d816
Nipa99 <34404699+Nipa99@users.noreply.github.com>
2025-10-19 20:07:11 -0600

    Update Program.cs colores en terminal

--- Program.cs
+++ Program.cs
@@ -5,8 +5,17 @@ class Program
 {
     static void Main(string[] args)
     {
+
+                // Cambiar color de fondo y texto
+        Console.BackgroundColor = ConsoleColor.DarkBlue;
+        Console.ForegroundColor = ConsoleColor.Yellow;
+
+        Console.Clear(); // Importante para aplicar el color de fondo en toda la pantalla
+        
         Console.WriteLine("Hola Mundo!");
         Console.WriteLine("Hello world");
+        
+
         Console.ReadLine(); // Para que la consola no se cierre inmediatamente
     }
 }
