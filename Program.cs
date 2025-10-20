ade4746e184db08b8323c77891e24a4ee0589270
Pablo <afmpablo@outlook.com>
2025-10-19 19:38:00 -0600

    Modificacion estructura codigo

--- Program.cs
+++ Program.cs
@@ -1,2 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hola Mundo!");
+        Console.ReadLine(); // Para que la consola no se cierre inmediatamente
+    }
+}
